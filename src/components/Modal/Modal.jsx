72127ce7e67a280d7f389fddaf2c995c2081c315
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-15 12:16:58 +0200

    modal content css

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import "./modal.css"
 import photoModal from "/src/assets/images/image-rules.svg"
+import croix from "/src/assets/images/icon-close.svg"
 export default function Modal() {
     const [isOpen, setIsOpen] = useState(false)
     const toggleModal = () =>{
-        setIsOpen(prev=>!prev)
+        setIsOpen(ferme=>!ferme)
     }
     return(
         <>
@@ -12,8 +13,14 @@ export default function Modal() {
             <p className="modalP" onClick={toggleModal}>RULES</p>
         </div>
         {isOpen &&(
-              <div className="modalOverlay" onClick={toggleModal}>
-                <div className="modalContent" >
+              <div className="modalOverlay" onClick={toggleModal} >
+                {/* POUR EVITER QU'EN CLIQUANT SUR L'IMAGE LE MODAL DISPARAIT */}
+                <div className="modalContent"  >
+                    <div className="divRules">
+                        <p className="rulesP">RULES</p>
+                        <img className="rulesImg" src={croix} alt="" />
+                    </div>
+                    
                     <img className="modalImg" src={photoModal} alt=""/>
                  </div>
              </div>

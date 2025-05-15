9cf73e886f204e14c7b93da1365c301f83dc76d4
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-15 11:19:29 +0200

    modal html

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./modal.css"
+
+export default function Modal() {
+    return(
+        <>
+        <div className="divModal">
+            <p className="modalP">RULES</p>
+        </div>
+        </>
+    )
+}

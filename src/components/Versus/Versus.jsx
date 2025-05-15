2baf6fedf8f3bd5771a8ffab300d5188de9e8d90
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-15 14:39:29 +0200

    component versus

--- src/components/Versus/Versus.jsx
+++ src/components/Versus/Versus.jsx
@@ -5,18 +5,32 @@ import { ciseaux, feuille, pierre } from "../Jeu/Jeu";
 export default function Versus({pick}) {
     return(
         <>
-        <div>
-            <div>
-                <div><h2>YOU PICKED</h2></div>
-                <div><img src={ciseaux} alt="" /></div>
+        <div className="divVersus">
+            <div className="versusDiv1">
+                <div className="versusDiv1Div1">
+                    <h2 className="versusDiv1Div1H2">YOU PICKED</h2>
+                </div>
+                <div className="versusDiv1Div2">
+                    <button className="bouton2Versus"> <img className="versusDiv1Div2Img" src={ciseaux} alt="" /></button>
+                    
+                </div>
             </div>
-            <div>
-                <div></div>
-                <div><button onClick={pick}>PLAY AGAIN</button></div>
+            <div className="versusDiv2">
+                <div className="versusDiv2Div1">
+                    <h1 className="versusDiv2Div1H1">YOU LOSE</h1>
+                </div>
+                <div className="versusDiv2Div2">
+                    <button className="versusDiv2Div2Btn" onClick={pick}>PLAY AGAIN</button>
+                </div>
             </div>
-            <div>
-                <div></div>
-                <div></div>
+            <div className="versusDiv3">
+                <div className="versusDiv3Div1">
+                    <h2 className="versusDiv3Div1H2">THE HOUSE PICKED</h2>
+                </div>
+                <div className="versusDiv3Div2">
+                    <button className="bouton3Versus"><img className="versusDiv3Div2Img" src={pierre} alt="" /></button>
+                    
+                </div>
             </div>
         </div>
         </>

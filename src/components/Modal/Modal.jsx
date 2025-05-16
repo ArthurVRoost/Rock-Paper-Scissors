93ea09fc41372ce68dddd4d3bb1ff01de56c26e0
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-16 10:13:01 +0200

    commentaires modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,14 @@
+// IMPORTS
 import { useState } from "react";
 import "./modal.css"
 import photoModal from "/src/assets/images/image-rules.svg"
 import croix from "/src/assets/images/icon-close.svg"
+
+// FONCTIONS
 export default function Modal() {
+    // CONST 
     const [isOpen, setIsOpen] = useState(false)
+    // TOGGLE
     const toggleModal = () =>{
         setIsOpen(ferme=>!ferme)
     }
@@ -12,6 +17,7 @@ export default function Modal() {
         <div className="divModal">
             <p className="modalP" onClick={toggleModal}>RULES</p>
         </div>
+        {/* EVALUATION CONDITIONELLE */}
         {isOpen &&(
               <div className="modalOverlay" onClick={toggleModal} >
                 <div className="modalContent"  >
